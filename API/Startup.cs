712ecc611af32d1be0a30b384ac3eb745e7e16d2
Mihai Bargau <mihaibargau@gmail.com>
2021-01-15 18:25:28 +0200

    add angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,6 +25,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             /*
             services.AddSwaggerGen(c =>
             {
@@ -48,6 +49,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
